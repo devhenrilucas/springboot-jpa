fb48447973ee940f486d6e39b0bb5c41755dba2b
devhenrilucas <lucazsbs15@gmail.com>
2026-02-20 17:42:49 -0300

    Service layer, component registration

--- src/main/java/com/learning/course/repositories/UserRepository.java
+++ src/main/java/com/learning/course/repositories/UserRepository.java
@@ -3,9 +3,4 @@ package com.learning.course.repositories;
 import com.learning.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-
-
-
-}
+public interface UserRepository extends JpaRepository<User, Long> { }
