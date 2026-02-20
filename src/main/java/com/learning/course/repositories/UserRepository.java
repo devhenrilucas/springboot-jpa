5895086bfd7320ded9ed75c3ec17ba0b207ea03a
devhenrilucas <lucazsbs15@gmail.com>
2026-02-20 16:04:06 -0300

    JPA repository, dependecy injection, database config

---
+++ src/main/java/com/learning/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.learning.course.repositories;
+
+import com.learning.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+
+}
